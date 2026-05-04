89b809872798a7ec76064bef8f8a59352554465f
Phan Anh Tuan <166937947+pa1tJj@users.noreply.github.com>
2026-05-05 01:01:06 +0700

    Delete springredis_object_handmade directory

--- springredis_object_handmade/src/main/java/com/jone/springredis/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jone.springredis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jone.springredis.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
-	public UserEntity findByUsernameAndPassword(String username, String password);
-}
